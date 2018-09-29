fb6073bec7947c0121f6a99af5355fb7ac5c532c
Pavel <pavel.bogdanov99@bk.ru>
2018-09-29 15:47:05 +0700

    ещё один коммит

--- js/main.js
+++ js/main.js
@@ -1,3 +1,80 @@
-$(document).ready(function() {
-	
-});
+// let num = 50;
+// if (num < 49){
+// 	console.log('1');
+// } 
+// else if (num == 49) {
+// console.log('2');
+// }
+// else {
+// console.log('3');
+// }
+
+// (num == 50)?	console.log('1') : console.log('2'); 
+
+// var num = 120;
+// switch (++num) {
+// 	case 100:
+// 	console.log(num); 
+// 	break;
+// 	case 120:
+// 	console.log(num); 
+// 	break;
+// 		case 120:
+// 	console.log(num); 
+// 	break;	
+// default: 
+// 	console.log(num); 
+// 	break;	
+// }
+
+
+// while( num<150){
+  	// console.log(num);
+// }
+
+// do{
+// 	console.log(num);
+// 	num++;
+// }
+// while(num < 130);
+
+// for( i= 110; i <= num; i++){
+// console.log(i);
+// }
+
+
+// let money = prompt("Ваш бюджет?");
+// let name = prompt("Название вашего магазина?");
+let time = 43; 
+// let mainList = {
+// 	budget: money,
+// 	shopName: name,
+// 	shopGoods: [],
+// 	employers: {}, 
+// 	open: false
+// } 
+// for (let i = 0; i < 3; i++){
+
+// 	let a = prompt("Какой тип товаров будем продавать?");
+// 	  if ((typeof(a)) === 'string' &&  (typeof(a)) === null && a !='' && a.length < 10  )  {
+//    console.log('Все равно!');
+//    mainList.shopGoods[i] = a; 
+// 	  }
+// }
+// 
+if (time < 0) {
+ console.log('Такого не может быть');
+} else if  (time > 8 && time < 20 ) {
+ console.log('Время работать');
+ } else if (time < 24 ){
+   console.log('Уже слишком поздно');
+ } else	{
+ 	 console.log('В сутках всего 24 часа :(');
+ }
+
+
+
+
+// alert(mainList / 30);
+
+// console.log(mainList);
